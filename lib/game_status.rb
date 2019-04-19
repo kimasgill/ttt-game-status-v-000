2e978eb7998f89c38246515bcfab8feb1291111a
Kimberly Asgill <kimberlyasgill@gmail.com>
2019-04-19 00:46:02 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,26 @@
+require 'pry'
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5],  
+  [6,7,8],  
+  [0,3,6],  
+  [1,4,7],  
+  [2,5,8],  
+  [0,4,8],  
+  [2,4,6]   
+  ]
+  
+  def won?(board)
+    WIN_COMBINATIONS.select do |win_combo|
+   board[win_combo[0]] == board[win_combo[1] == board[win_combo[2]] 
+   board[win_combo[3]] == board[win_combo[4]]
+   board[win_combo[2]] == board[win_combo[3]]
+   board[win_combo[3]] == board[win_combo[4]]
+  end
+ end
