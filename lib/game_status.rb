9f6f58f4eede6a5fc93258a24b9d812293bf2dd3
Kimberly Asgill <kimberlyasgill@gmail.com>
2019-04-19 00:55:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,11 +25,7 @@ WIN_COMBINATIONS = [
   # end
 # end
 
-WIN_COMBINATIONS.each do |win_combo|
-  
-win_combination in WIN_COMBINATIONS
-  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-  # grab each index from the win_combination that composes a win.
+WIN_COMBINATIONS.each do |win_combination|
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
